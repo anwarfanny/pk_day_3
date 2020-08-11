9d59c2f7e32ecc0e63c31f1a505229ee4d413817
fanni <anwarainul>
2020-08-11 16:16:07 +0700

    latihan java script

---
+++ script.js
@@ -0,0 +1,9 @@
+window.alert("hello word")
+window.prompt("siapa nama kamu?")
+
+document.getElementById("element")
+console.log(document.getElementById("Element"))
+
+document.body.innerHTML += "<h1>hello word</h1>"
+document.body.innerHTML = document.body.innerHTML + "hello word</h1>"
+document.getElementById("element").innerHTML = "<h3>hello</h3>"
